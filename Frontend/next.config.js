6ef82648fce0ed2c35540aa4889ca479e5300e97
devjorgeluis <jorgencnc0608@gmail.com>
2024-06-14 01:10:28 +0800

    reset next config

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,9 +1,5 @@
-const prod = process.env.NODE_ENV === 'production'
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,
-  disable: prod ? false : true,
-  skipWaiting: true
 });
 
 module.exports = withPWA({

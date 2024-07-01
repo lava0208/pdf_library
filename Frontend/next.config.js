a6664219a409081543c175cf077c0ba7467ff73b
devjorgeluis <jorgencnc0608@gmail.com>
2024-07-01 23:28:15 +0800

    update next config

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,5 +1,9 @@
+const prod = process.env.NODE_ENV === 'production'
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  disable: prod ? false : true,
+  skipWaiting: true
 });
 
 module.exports = withPWA({
